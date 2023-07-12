bf5b9ab59e274b269e0817485b05652bdf9ed59a
qiyoudaoyi <qiyoudaoyi@gmail.com>
2023-07-12 22:59:21 +0800

    First Commit

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
+
+/* TODO: */
+/*	1.
+/********/
+
+func main() {
+	dsn := "postgres://postgres:@localhost:5432/test?sslmode=disable"
+	// dsn := "unix://user:pass@dbname/var/run/postgresql/.s.PGSQL.5432"
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/eesoymilk/health-statistic-api
+
+go 1.20
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/uptrace/bun v1.1.14 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
