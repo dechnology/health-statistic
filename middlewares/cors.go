04d5cb45389911e8b0f9025a76fa3b043bd53f48
qiyoudaoyi <qiyoudaoyi@gmail.com>
2023-07-27 13:23:45 +0800

    Change CORS policy

---
+++ middlewares/cors.go
@@ -0,0 +1,33 @@
+package middlewares
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddleware() gin.HandlerFunc {
+	c := cors.Config{
+		AllowAllOrigins: true, // this allows all origins
+		AllowOrigins:    []string{"*"},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"PATCH",
+			"DELETE",
+			"HEAD",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Accept",
+			"User-Agent",
+			"Content-Length",
+			"Content-Type",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-length"},
+	}
+
+	return cors.New(c)
+}
